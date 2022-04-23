4225dc85c538c7d0a9bbbc8c094d43a8ba35109e
DrTCortes <thiagocortes@outlook.com.br>
2022-04-22 22:40:30 -0300

    React Calculator

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import reportWebVitals from './reportWebVitals';
 import Calculator from './main/Calculator';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -13,5 +12,3 @@ root.render(
     </div>
   </React.StrictMode>
 );
-
-reportWebVitals();
